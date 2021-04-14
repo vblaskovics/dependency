0eba52f4d8d814110940788736eb82a5c76fc8f5
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-14 16:05:45 +0200

    Use injector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserDemoComponent } from './user-demo/user-demo.component';
+import { UserDemoModule } from './user-demo/user-demo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserDemoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
