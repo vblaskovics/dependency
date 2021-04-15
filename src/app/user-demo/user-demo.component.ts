0e4e013a0c51ce6360c8ceb67f3bf73ca6e770da
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-15 09:39:01 +0200

    Add random service

--- src/app/user-demo/user-demo.component.ts
+++ src/app/user-demo/user-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { RandomService } from '../services/random.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,20 +8,27 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-demo.component.css'],
 })
 export class UserDemoComponent implements OnInit {
+  private userService: UserService;
   userName: string;
+  randomMsg: string;
 
   constructor(
-    private userService: UserService,
-    @Inject('API_URL') private apiUrl: string
-  ) {}
+    userService: UserService,
+    @Inject('API_URL') private apiUrl: string,
+    @Inject('RANDOM') private randomService: RandomService
+  ) {
+    this.userService = userService;
+  }
 
   signIn(): void {
     this.userService.setUser({
       name: 'Peter',
     });
+    this.randomMsg = `My random is: ${this.randomService.getRandom()}`;
     console.log('API URL', this.apiUrl);
     this.userName = this.userService.getUser().name;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
--- src/app/user-demo/user-demo.module.ts
+++ src/app/user-demo/user-demo.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import {CommonModule } from '@angular/common';
 
 import { UserService } from '../services/user.service';
+import { RandomService } from '../services/random.service';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     providers: [
-        // UserService,
-        {provide: UserService, useClass: UserService},
-        {provide: 'API_URL', useValue: 'http://localhost:4200'}
+        UserService,
+        // {provide: UserService, useClass: UserService},
+        {provide: 'API_URL', useValue: 'http://localhost:4200'},
+        {provide: 'RANDOM', useClass: RandomService},
     ],
     declarations: []
 })
